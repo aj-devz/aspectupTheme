053b893e5697476be437c99700ef1abd2dc7a9c6
Aj's Dev <160000744+aj-devz@users.noreply.github.com>
2024-11-06 06:40:48 +0800

    Update rvt.sh

--- rvt.sh
+++ rvt.sh
@@ -3,6 +3,6 @@ cd
 cd ..
 cd usr/etc
 rm bash.bashrc
-cd $HOME/aspectup-welcome/Revert
+cd $HOME/jarvis-welcome/Revert
 mv bash.bashrc $PREFIC/etc
-python $HOME/aspectup-welcome/Revert/Thanks.py
+python $HOME/jarvis-welcome/Revert/Thanks.py
